13cfa8bb48f1dcee761583a4fdb5ac6191451f89
emazack <em.zaccaria@gmail.com>
2022-01-05 14:45:19 +0100

    save and edit

--- main.js
+++ main.js
@@ -13,29 +13,30 @@ enterButton.addEventListener("click", function() {
             <div class="check">
                 <input type="checkbox" name="check-todo">
             </div>
-            <div class="text">
-                ${text}
-            </div>
+            <div class="text">${text}</div>
             <div class="buttons">
                 <button class="edit">
                     edit
+                </button>
+                <button class="save hidden">
+                    save
                 </button>
                 <button class="delete">
                     delete
                 </button>
             </div>
         </div>
-    `
+    `;
     todo();
 })
 
 function todo() {
-    const todo = document.querySelectorAll(".todo");
+    //const todo = document.querySelectorAll(".todo");
     const editButtons = document.querySelectorAll(".edit");
+    const saveButtons = document.querySelectorAll(".save");
     const deleteButtons = document.querySelectorAll(".delete");
     
     // remove todo
-
     deleteButtons.forEach(singleButton => {        
         singleButton.addEventListener("click", function (e) {
             let clickedButton = e.currentTarget;
@@ -45,4 +46,37 @@ function todo() {
     });
     
     // edit todo
+    editButtons.forEach(singleButton => {        
+        singleButton.addEventListener("click", function (e) {
+            let clickedButton = e.currentTarget;
+            let saveButton = clickedButton.nextElementSibling;
+            let divToEdit = clickedButton.closest(".buttons").previousElementSibling;
+            let textToEdit = divToEdit.innerHTML;
+            divToEdit.outerHTML = 
+            `
+            <input class="text-edit" type="text" value="${textToEdit}">
+            `;
+            saveButton.classList.remove("hidden");
+            clickedButton.classList.add("hidden");
+        })
+    });
+
+    //save todo
+    saveButtons.forEach(singleButton => {        
+        singleButton.addEventListener("click", function (e) {
+            let clickedButton = e.currentTarget;
+            let editButton = clickedButton.previousElementSibling;
+            let inputToSave = clickedButton.closest(".buttons").previousElementSibling;
+            let textToSave = inputToSave.value;
+            inputToSave.outerHTML = 
+            `
+            <div class="text">
+                ${textToSave}       
+            </div>
+            `;
+            editButton.classList.remove("hidden");
+            clickedButton.classList.add("hidden");
+        })
+    });
+
 }
